7334ce82ce43b7a8d2d2ae9d2e5d66871bc5b478
ambarish2012 <ambarish@shippable.com>
2018-05-17 11:18:07 -0700

    update sample

--- utilities/replace_deployment_strategy.sh
+++ utilities/replace_deployment_strategy.sh
@@ -3,5 +3,8 @@ CLUSTER_NAME=$1
 SERVICE_NAME=$2
 REPO_DIR=$3
 
+echo "deleting existing service"
 ${REPO_DIR}/utilities/delete_existing_ecs_service.sh ${CLUSTER_NAME} ${SERVICE_NAME}
-aws ecs create-service --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME} --cli-input-json file://${REPO_DIR}/specs/servicedefinition.json
+
+echo "creating new service"
+aws ecs create-service --cli-input-json file://${REPO_DIR}/specs/servicedefinition.json
--- utilities/upgrade_deployment_strategy.sh
+++ utilities/upgrade_deployment_strategy.sh
@@ -3,4 +3,4 @@ CLUSTER_NAME=$1
 SERVICE_NAME=$2
 REPO_DIR=$3
 
-aws ecs update-service --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME} --cli-input-json file://${REPO_DIR}/specs/servicedefinition.json --force-new-deployment true
+aws ecs update-service --cli-input-json file://${REPO_DIR}/specs/servicedefinition.json --force-new-deployment true
