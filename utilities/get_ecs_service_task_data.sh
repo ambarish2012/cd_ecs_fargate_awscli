92d9caab18d7786f03a8e18a2adb7efd918bda1c
ambarish2012 <ambarish@shippable.com>
2018-05-17 16:30:26 -0700

    update sample

--- utilities/get_ecs_service_task_data.sh
+++ utilities/get_ecs_service_task_data.sh
@@ -3,5 +3,4 @@ CLUSTER_NAME=$1
 SERVICE_NAME=$2
 
 TASK_DEFINITION_ID=$(aws ecs describe-services --cluster ${CLUSTER_NAME}  --service ${SERVICE_NAME} | jq ".services[0].events[1].message" | awk '{print $8}' | cut -d ')' -f 1)
-echo "TASK_DEFINITION_ID is "${TASK_DEFINITION_ID}
 aws ecs describe-tasks --cluster ${CLUSTER_NAME} --tasks ${TASK_DEFINITION_ID}
