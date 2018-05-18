54234415a5edb11709fa8d182b932ca626ab9b50
ambarish2012 <ambarish@shippable.com>
2018-05-17 17:41:23 -0700

    update sample

--- utilities/get_ecs_service_public_ip.sh
+++ utilities/get_ecs_service_public_ip.sh
@@ -4,6 +4,7 @@ SERVICE_NAME=$2
 REPO_DIR=$3
 
 TASK_DEFINITION_ID=$(aws ecs describe-services --cluster ${CLUSTER_NAME}  --service ${SERVICE_NAME} | jq ".services[0].events[1].message" | awk '{print $8}' | cut -d ')' -f 1)
+echo $TASK_DEFINITION_ID
 TASK_DATA=$(aws ecs describe-tasks --cluster ${CLUSTER_NAME} --tasks ${TASK_DEFINITION_ID})
 echo $TASK_DATA
 
