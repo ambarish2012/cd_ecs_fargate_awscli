4902ea5be11c707e9c3641a9912bc8fdcbb4ee5f
ambarish2012 <ambarish@shippable.com>
2018-05-17 18:06:14 -0700

    update sample

--- utilities/get_ecs_service_task_data.sh
+++ utilities/get_ecs_service_task_data.sh
@@ -2,6 +2,5 @@
 CLUSTER_NAME=$1
 SERVICE_NAME=$2
 
-
 TASK_ARN=$(aws ecs list-tasks --cluster ${CLUSTER_NAME}  --service ${SERVICE_NAME} | jq -r ".taskArns[0]")
 aws ecs describe-tasks --cluster ${CLUSTER_NAME} --tasks ${TASK_ARN}
