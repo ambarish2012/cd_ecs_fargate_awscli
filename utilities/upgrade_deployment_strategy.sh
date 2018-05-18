5ac32eb93d408206956a55856bf7c89ba7136583
ambarish2012 <ambarish@shippable.com>
2018-05-17 22:27:16 -0700

    update sample

--- utilities/upgrade_deployment_strategy.sh
+++ utilities/upgrade_deployment_strategy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 CLUSTER_NAME=$1
 SERVICE_NAME=$2
-REPO_DIR=$3
+TASK_DEFINITION=$3
 
-aws ecs update-service --cli-input-json file://${REPO_DIR}/specs/servicedefinition.json --force-new-deployment true
+aws ecs update-service --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME} --task-definition ${TASK_DEFINITION} --force-new-deployment
