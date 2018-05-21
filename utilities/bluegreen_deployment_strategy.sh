a159aaed92974b3058b863bc47908a552dcc765e
ambarish2012 <ambarish@shippable.com>
2018-05-21 12:55:55 -0700

    update sample

--- utilities/bluegreen_deployment_strategy.sh
+++ utilities/bluegreen_deployment_strategy.sh
@@ -1,62 +1,62 @@
 #!/bin/bash
 function bluegreen_deployment_strategy {
-  CLUSTER_NAME=$1
-  SERVICE_NAME=$2
-  REPO_DIR=$3
-  DESIRED_TASK_COUNT=$4
-  VALIDATION_TASK_SCRIPT=$5
+    CLUSTER_NAME=$1
+    SERVICE_NAME=$2
+    REPO_DIR=$3
+    DESIRED_TASK_COUNT=$4
+    VALIDATION_TASK_SCRIPT=$5
 
-  rm $JOB_PREVIOUS_STATE/serviceId.env
+    rm $JOB_PREVIOUS_STATE/serviceId.env
 
-  # Generate green service name
-  if [ -f $JOB_PREVIOUS_STATE/serviceId.env ]; then
-    . $JOB_PREVIOUS_STATE/serviceId.env
-    EXISTING_BLUE_SERVICE_NAME=${SERVICE_NAME}${serviceId}
-    let "serviceId++"
-  else
-    serviceId=1
-  fi
-
-  GREEN_SERVICE_NAME=${SERVICE_NAME}${serviceId}
-
-  # Deploy green service
-  echo "Creating green service"${GREEN_SERVICE_NAME}
-  aws ecs create-service --cli-input-json file://${REPO_DIR}/specs/servicedefinition.json
-  retVal=$?
-  if [ $retVal -ne 0 ]; then
-    echo "creation of green service failed"
-    return $retVal
-  fi
-
-  # Check status of green service
-  ${REPO_DIR}/utilities/check_service_stability.sh ${CLUSTER_NAME} ${GREEN_SERVICE_NAME} ${DESIRED_TASK_COUNT} 5
-  retVal=$?
-  if [ $retVal -ne 0 ]; then
-    ${REPO_DIR}/utilities/delete_existing_ecs_service.sh ${CLUSTER_NAME} ${GREEN_SERVICE_NAME}
-    return $retVal
-  fi
-
-  # Run any validation task scripts
-  if [ -f ${VALIDATION_TASK_SCRIPT} ]; then
-    . ${VALIDATION_TASK_SCRIPT}
-  fi
-
-  # Increment and persist serviceId
-  echo "serviceId=${serviceId}" > serviceId.env
-  shipctl copy_file_to_state serviceId.env
-
-  # Delete service if it existed at the very first deployment
-  if [ "${serviceId}" -eq 1 ]; then
-    # Check if blue service was already running
-    RUNNING_TASK_COUNT=$(aws ecs describe-services --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME} | jq ".services[0].runningCount")
-    if [ "${RUNNING_TASK_COUNT}" -eq 1 ]; then
-      ${REPO_DIR}/utilities/delete_existing_ecs_service.sh ${CLUSTER_NAME} ${SERVICE_NAME}
+    # Generate green service name
+    if [ -f $JOB_PREVIOUS_STATE/serviceId.env ]; then
+      . $JOB_PREVIOUS_STATE/serviceId.env
+      EXISTING_BLUE_SERVICE_NAME=${SERVICE_NAME}${serviceId}
+      let "serviceId++"
+    else
+      serviceId=1
     fi
-  else
-    # Delete previously deployed blue service
-    ${REPO_DIR}/utilities/delete_existing_ecs_service.sh ${CLUSTER_NAME} ${EXISTING_BLUE_SERVICE_NAME}
-    echo "Deployment is complete"
-  fi
 
-  return 0
+    GREEN_SERVICE_NAME=${SERVICE_NAME}${serviceId}
+
+    # Deploy green service
+    echo "Creating green service"${GREEN_SERVICE_NAME}
+    aws ecs create-service --cli-input-json file://${REPO_DIR}/specs/servicedefinition.json
+    retVal=$?
+    if [ $retVal -ne 0 ]; then
+      echo "creation of green service failed"
+      return $retVal
+    fi
+
+    # Check status of green service
+    ${REPO_DIR}/utilities/check_service_stability.sh ${CLUSTER_NAME} ${GREEN_SERVICE_NAME} ${DESIRED_TASK_COUNT} 5
+    retVal=$?
+    if [ $retVal -ne 0 ]; then
+      ${REPO_DIR}/utilities/delete_existing_ecs_service.sh ${CLUSTER_NAME} ${GREEN_SERVICE_NAME}
+      return $retVal
+    fi
+
+    # Run any validation task scripts
+    if [ -f ${VALIDATION_TASK_SCRIPT} ]; then
+      . ${VALIDATION_TASK_SCRIPT}
+    fi
+
+    # Increment and persist serviceId
+    echo "serviceId=${serviceId}" > serviceId.env
+    shipctl copy_file_to_state serviceId.env
+
+    # Delete service if it existed at the very first deployment
+    if [ "${serviceId}" -eq 1 ]; then
+      # Check if blue service was already running
+      RUNNING_TASK_COUNT=$(aws ecs describe-services --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME} | jq ".services[0].runningCount")
+      if [ "${RUNNING_TASK_COUNT}" -eq 1 ]; then
+        ${REPO_DIR}/utilities/delete_existing_ecs_service.sh ${CLUSTER_NAME} ${SERVICE_NAME}
+      fi
+    else
+      # Delete previously deployed blue service
+      ${REPO_DIR}/utilities/delete_existing_ecs_service.sh ${CLUSTER_NAME} ${EXISTING_BLUE_SERVICE_NAME}
+      echo "Deployment is complete"
+    fi
+
+    return 0
 }
--- utilities/check_service_stability.sh
+++ utilities/check_service_stability.sh
@@ -1,33 +1,36 @@
 #!/bin/bash -e
-CLUSTER_NAME=$1
-SERVICE_NAME=$2
-DESIRED_TASK_COUNT=$3
-MINUTES_TO_POLL=$4
 
-SUCCESS=1
-RUNNING_TASK_COUNT=0
-format='%Y%m%d%H%M%S'
-currDate=$(date +${format})
-finalDate=$(date -d 'now + '${MINUTES_TO_POLL}' minutes' +${format})
+function check_service_stability {
+    CLUSTER_NAME=$1
+    SERVICE_NAME=$2
+    DESIRED_TASK_COUNT=$3
+    MINUTES_TO_POLL=$4
 
-while [ "${currDate}" -lt "${finalDate}" ]
-do
-    RUNNING_TASK_COUNT=$(aws ecs describe-services --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME} | jq ".services[0].runningCount")
-    echo "Running task count "${RUNNING_TASK_COUNT}
+    SUCCESS=1
+    RUNNING_TASK_COUNT=0
+    format='%Y%m%d%H%M%S'
+    currDate=$(date +${format})
+    finalDate=$(date -d 'now + '${MINUTES_TO_POLL}' minutes' +${format})
 
-    if [ "${RUNNING_TASK_COUNT}" -eq "${DESIRED_TASK_COUNT}" ]; then
-        SUCCESS=0
-        break
+    while [ "${currDate}" -lt "${finalDate}" ]
+    do
+        RUNNING_TASK_COUNT=$(aws ecs describe-services --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME} | jq ".services[0].runningCount")
+        echo "Running task count "${RUNNING_TASK_COUNT}
+
+        if [ "${RUNNING_TASK_COUNT}" -eq "${DESIRED_TASK_COUNT}" ]; then
+            SUCCESS=0
+            break
+        fi
+
+        currDate=$(date +${format})
+        sleep 2
+    done
+
+    if [ "${SUCCESS}" -neq 0 ]; then
+      echo "Desired count of tasks has not been reached in time specified, please check ECS service event logs for more details"
+    else
+      echo "Deployment is complete"
     fi
 
-    currDate=$(date +${format})
-    sleep 2
-done
-
-if [ "${SUCCESS}" -neq 0 ]; then
-  echo "Desired count of tasks has not been reached in time specified, please check ECS service event logs for more details"
-else
-  echo "Deployment is complete"
-fi
-
-return ${SUCCESS}
+    return ${SUCCESS}
+}
