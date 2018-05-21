abce74d4cefe353bfc3628518ef02cd587d161d1
ambarish2012 <ambarish@shippable.com>
2018-05-21 14:48:58 -0700

    update sample

--- utilities/upgrade_deployment_strategy.sh
+++ utilities/upgrade_deployment_strategy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 CLUSTER_NAME_ARG=$1
 SERVICE_NAME_ARG=$2
-TASK_DEFINITION_ARg=$3
+TASK_DEFINITION_ARG=$3
 
 aws ecs update-service --cluster ${CLUSTER_NAME_ARG} --service ${SERVICE_NAME_ARG} --task-definition ${TASK_DEFINITION_ARG} --force-new-deployment
