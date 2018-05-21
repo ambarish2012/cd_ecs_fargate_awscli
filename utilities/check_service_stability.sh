68390ae4decd3899cd38f420e96187fa152243cb
ambarish2012 <ambarish@shippable.com>
2018-05-21 12:28:12 -0700

    update sample

--- utilities/check_service_stability.sh
+++ utilities/check_service_stability.sh
@@ -4,19 +4,19 @@ SERVICE_NAME=$2
 DESIRED_TASK_COUNT=$3
 MINUTES_TO_POLL=$4
 
-SUCCESS=0
+SUCCESS=1
 RUNNING_TASK_COUNT=0
 format='%Y%m%d%H%M%S'
 currDate=$(date +${format})
 finalDate=$(date -d 'now + '${MINUTES_TO_POLL}' minutes' +${format})
 
-while [ "$currDate" -lt "$finalDate" ]
+while [ "${currDate}" -lt "${finalDate}" ]
 do
     RUNNING_TASK_COUNT=$(aws ecs describe-services --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME} | jq ".services[0].runningCount")
     echo "Running task count "${RUNNING_TASK_COUNT}
 
-    if [ "$RUNNING_TASK_COUNT" -eq "$DESIRED_TASK_COUNT" ]; then
-        SUCCESS=1
+    if [ "${RUNNING_TASK_COUNT}" -eq "${DESIRED_TASK_COUNT}" ]; then
+        SUCCESS=0
         break
     fi
 
@@ -24,10 +24,10 @@ do
     sleep 2
 done
 
-if [ "$SUCCESS" -eq 0 ]; then
+if [ "${SUCCESS}" -neq 0 ]; then
   echo "Desired count of tasks has not been reached in time specified, please check ECS service event logs for more details"
-  exit 1
 else
   echo "Deployment is complete"
-  exit 0
 fi
+
+return ${SUCCESS}
---
+++ utilities/get_green_service_name.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+SERVICE_NAME=$1
+
+# Generate green service name
+. $JOB_STATE/serviceId.env
+echo ${SERVICE_NAME}${serviceId}
